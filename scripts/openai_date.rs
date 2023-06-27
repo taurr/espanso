b6c164bddbd243c392c40cf661aba4f55a08a3c5
Johnny T. Vestergaard <vestergaard.johnny.ext@siemensgamesa.com>
2023-06-27 10:52:23 +0200

    change triggers

--- scripts/openai_date.rs
+++ scripts/openai_date.rs
@@ -8,12 +8,10 @@
 //! openai = "1.0.0-alpha.12"
 //! tokio = "1.28.2"
 //! chrono = "0.4.19"
-//! dotenv = "0.15.0"
 //! ```
 
 use anyhow::*;
 use chrono::Local;
-use dotenv::dotenv;
 use openai::{
     chat::{ChatCompletion, ChatCompletionMessage, ChatCompletionMessageRole},
     set_key,
@@ -26,9 +24,6 @@ async fn main() -> Result<()> {
     let now = Local::now();
     let now = now.format("%Y/%m/%d %H:%M:%S");
 
-    //println!("Query is: {}", &args);
-
-    dotenv().ok();
     set_key(env::var("OPENAI_KEY").context("OPENAI_KEY")?);
 
     let mut messages = vec![ChatCompletionMessage {
@@ -36,14 +31,15 @@ async fn main() -> Result<()> {
         content: Some(format!(
             r#"You act as a natural language date/time parser.
 Rules:
-- You must parse the input into a date/time, or give the string "Unknown"
-- Any input not resulting in a date/time string, must be ignored and answered with the string "Unknown"
-- Only ever answer with a complete date/time string, or the string "Unknown"
-- Do not provide any additional information nor error context, only the date/time string or the string "Unknown"
+- You must parse the input into a date/time if possible
+- Any input not resulting in a date/time string, must be ignored and answered with the failure "Unknown"
+- Only answer with a complete date/time string, or the failure "Unknown"
+- Do not provide any additional information nor error context
 - For date/time, by default use the formats "YYYY-MM-DD" or "YYYY-MM-DD HH:MM:SS"
 - Check your answer before submitting it
 
 Time now: {now}
+
 "#
         )),
         name: None,
